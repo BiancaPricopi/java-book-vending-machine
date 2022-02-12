8b80df60e9d973e2eafd479bf0db6256ca077d3f
Pricopi Bianca <75115098+BiancaPricopi@users.noreply.github.com>
2022-02-12 18:42:26 +0200

    reader methods

--- BookVendingMachine/src/BookVendingMachine/Reader.java
+++ BookVendingMachine/src/BookVendingMachine/Reader.java
@@ -2,5 +2,18 @@ package BookVendingMachine;
 
 public class Reader
 {
+	Book book = new Book();
+	public void borrowBook()
+	{
+		
+	}
+	public void returnBook()
+	{
+		
+	}
+	public void checkTimeToRead()
+	{
+		System.out.println("The loan period for borrowed book is 31 days.");
+	}
 
 }
