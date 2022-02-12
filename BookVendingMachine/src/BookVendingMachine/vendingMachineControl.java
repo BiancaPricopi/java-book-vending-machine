94886304c5f5446ab479cceae6df64d1722bf3b5
Pricopi Bianca <75115098+BiancaPricopi@users.noreply.github.com>
2022-02-12 18:49:24 +0200

    vending machine control antiquarian or reader

---
+++ BookVendingMachine/src/BookVendingMachine/vendingMachineControl.java
@@ -0,0 +1,13 @@
+package BookVendingMachine;
+
+public class vendingMachineControl 
+{
+	public Antiquarian getAntiquarian()
+	{
+		return new Antiquarian();
+	}
+	public Reader getReader()
+	{
+		return new Reader();
+	}
+}
