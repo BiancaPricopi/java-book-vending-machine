b809a7db58972dcaf126df2434b27102a3f463cf
Pricopi Bianca <75115098+BiancaPricopi@users.noreply.github.com>
2022-02-13 23:26:13 +0200

    reader button implemented

--- BookVendingMachine/src/BookVendingMachine/ReaderButton.java
+++ BookVendingMachine/src/BookVendingMachine/ReaderButton.java
@@ -2,5 +2,13 @@ package BookVendingMachine;
 
 public class ReaderButton 
 {
-
+	readerCommand command;
+	public ReaderButton(readerCommand newCommand)
+	{
+		this.command = newCommand;
+	}
+	public void press()
+	{
+		this.command.execute();
+	}
 }
