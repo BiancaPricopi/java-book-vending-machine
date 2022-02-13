c612364b8b04dd1ee42e37bdf4e9b88e3beb4570
Pricopi Bianca <75115098+BiancaPricopi@users.noreply.github.com>
2022-02-13 23:23:57 +0200

    antiquarian button implemented

--- BookVendingMachine/src/BookVendingMachine/AntiquarianButton.java
+++ BookVendingMachine/src/BookVendingMachine/AntiquarianButton.java
@@ -2,5 +2,13 @@ package BookVendingMachine;
 
 public class AntiquarianButton 
 {
-
+	AntiquarianCommand command;
+	public AntiquarianButton(AntiquarianCommand newCommand)
+	{
+		this.command = newCommand;
+	}
+	public void press()
+	{
+		this.command.execute();
+	}
 }
