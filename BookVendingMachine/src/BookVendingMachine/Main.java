01432291ce2adc1397ead5cb21a518b31f1e7f59
Pricopi Bianca <75115098+BiancaPricopi@users.noreply.github.com>
2022-02-17 13:58:35 +0200

    antiquarian added first book

--- BookVendingMachine/src/BookVendingMachine/Main.java
+++ BookVendingMachine/src/BookVendingMachine/Main.java
@@ -5,8 +5,14 @@ public class Main
 
 	public static void main(String[] args)
 	{
-		Book book1 = new Book();
 		Antiquarian antiquarian = new Antiquarian();
+		Book book1 = new Book();
+		book1.setTitle("Cronica pasarii arc");
+		book1.setAuthor("Haruki Murakami");
+		book1.setPages(688);
+		AddBook antiquarianAddBook = new AddBook(antiquarian, book1);
+		AntiquarianButton antiquarianButton = new AntiquarianButton(antiquarianAddBook);
+		antiquarianButton.press();
 
 	}
 
