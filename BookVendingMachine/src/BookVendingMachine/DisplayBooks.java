26ffc769485291aefa515f7640c12e208a26d908
Pricopi Bianca <75115098+BiancaPricopi@users.noreply.github.com>
2022-02-19 17:05:00 +0200

    [reader] display books command implemented

--- BookVendingMachine/src/BookVendingMachine/DisplayBooks.java
+++ BookVendingMachine/src/BookVendingMachine/DisplayBooks.java
@@ -3,10 +3,13 @@ package BookVendingMachine;
 public class DisplayBooks implements readerCommand
 {
 	Reader reader;
-	@Override
-	public void execute() {
-		// TODO Auto-generated method stub
-		
+	public DisplayBooks(Reader newReader)
+	{
+		this.reader = newReader;
 	}
-
+	@Override
+	public void execute() 
+	{
+		this.reader.displayAvailableBooks();
+	}
 }
